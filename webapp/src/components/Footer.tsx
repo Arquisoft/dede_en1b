cd7246566c1566633e455ba599ad0ed76868e620
LuisMi Alonso <60442261+lumialfe@users.noreply.github.com>
2022-03-07 17:13:54 +0100

    Updated Footer and Added Icons

--- webapp/src/components/Footer.tsx
+++ webapp/src/components/Footer.tsx
@@ -1,9 +1,28 @@
+import { AppBar, Toolbar, Typography, Link } from "@mui/material";
+import { BottomNavigation, BottomNavigationAction } from "@material-ui/core";
+import GroupRoundedIcon from '@mui/icons-material/GroupRounded';
+import CircleRoundedIcon from '@mui/icons-material/CircleRounded';
+import CodeRoundedIcon from '@mui/icons-material/CodeRounded';
+import { useState } from "react";
+import { BorderAll } from "@material-ui/icons";
+
 function Footer() : JSX.Element {
+    const [selected, setSelected] = useState(0);
      return (
        <>
-            <AppBar>
-                <Toolbar>
-                    <Typography
+            <BottomNavigation
+                style={{ width: "100%",
+                    backgroundColor: "lavender",
+                    marginTop: "5%",
+                    marginBottom: "5%",
+                    height: "200px"
+                }}
+                showLabels
+            >
+                <BottomNavigationAction label="About Us" icon={<GroupRoundedIcon />} href="/about_us" />
+                <BottomNavigationAction label="SOLID" icon={<CircleRoundedIcon />} href="https://solidproject.org/" />
+                <BottomNavigationAction label="Source code" icon={<CodeRoundedIcon />} href="https://github.com/arquisoft/dede_en_01b" />
+                    {/* <Typography
                         variant="h6"
                         noWrap
                         component="div"
@@ -35,9 +54,8 @@ function Footer() : JSX.Element {
                         sx={{ display: { xs: 'none', sm: 'block' } }}
                     >
                         <Link href="https://github.com/arquisoft/dede_en_01b">Source code</Link>
-                    </Typography>
-                </Toolbar>
-            </AppBar>
+                    </Typography> */}
+                </BottomNavigation>
        </>
      );
 }
