7b2f36a08af226795eacab086fa2c9ac018c4f4a
Daniel <danielarezdiaz@gmail.com>
2022-03-08 12:22:01 +0100

    Add files

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,41 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
-import Container from '@mui/material/Container';
-import EmailForm from './components/EmailForm';
-import Welcome from './components/Welcome';
-import ProductList from './components/ProductList';
-import  {getProducts,getCart} from './api/api';
-import {Product, ItemCart} from './shared/shareddtypes';
+
 import './App.css';
 import { TextField } from '@mui/material';
+import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
+import Login  from "./components/Login";
+import MainProducts from "./components/MainProducts";
+
 
 function App(): JSX.Element {
 
-  const [products,setProducts] = useState<Product[]>([]);
-  const [cart,setCart] = useState<ItemCart[]>([]);
-
-  const refreshProductList = async () => {
-    setProducts(await getProducts());
-  }
-
-  const refreshCartList = async () => {
-    setCart(await getCart());
-  }
-
-  useEffect(()=>{
-    refreshProductList();
-    refreshCartList();
-  },[]);
+  
 
   return (
     <>
-      <Container maxWidth="sm">
-        <ProductList products={products}/>
-        <Link href="https://github.com/arquisoft/dede_en_01b">Source code</Link>
-        {console.log(cart)}
-        {cart.map(itemCart => <Box key={itemCart.product.name}>{itemCart.product.name + itemCart.quantity}</Box>)}
-      </Container>
+      <Router>
+        <Routes>
+          <Route path='/' element={<MainProducts/>} />
+          <Route  path="/login" element={<Login/>} />
+
+        </Routes>
+    </Router>
     </>
   );
 }
