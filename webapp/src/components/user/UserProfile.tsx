806098741dc32a62d49b1dfb3e76bda381cab9f7
Luis Miguel Alonso <UO270139@uniovi.es>
2022-04-02 02:32:51 +0200

    Retrieve VCard with axios

--- webapp/src/components/user/UserProfile.tsx
+++ webapp/src/components/user/UserProfile.tsx
@@ -19,10 +19,18 @@ import { useEffect, useState } from 'react';
 import OrderCard from "./OrderCard";
 import { Order } from "../../shared/shareddtypes";
 
+import axios from "axios";
 
 export default function UserProfile() {
     const { session } = useSession();
     const webId = session.info.webId as string;
+    
+    const username = webId.substring(8, webId.length - 27);
+
+    axios.get("https://" + username + ".solidcommunity.net/profile/card#address")
+        .then(res => {
+            console.log(res.data);
+    })
 
     //Order management:
     const [orders, setOrders] = useState<Order[]>([]);
@@ -60,10 +68,14 @@ export default function UserProfile() {
             >
                 <Typography id="pageTitle" variant="h3">
                     <span>Welcome, </span>
-                    <Text properties={[
+                    {/* <Text properties={[
                         "http://www.w3.org/2006/vcard/ns#fn",
                         "http://xmlns.com/foaf/0.1/name",
-                    ]} />
+                    ]} /> */}
+                    {/* <Text properties={[
+                        "http://www.w3.org/2006/vcard/ns#fn",
+                        "http://www.w3.org/2006/vcard/ns#",
+                    ]} /> */}
                 </Typography>
 
                 <Divider id="mainDivider" orientation="horizontal" flexItem />
@@ -79,7 +91,7 @@ export default function UserProfile() {
                 >
 
                     {orders.map(order => (
-                        <OrderCard order = {order}/>
+                        <OrderCard order={order} />
                     ))}
                 </Stack>
             </CombinedDataProvider>
