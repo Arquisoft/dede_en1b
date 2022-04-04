bf0fbf69cc762017b305ae64e1c7f464412ad7c5
Sebastian L.H <66498000+sebaslh01@users.noreply.github.com>
2022-04-04 23:56:22 +0200

    Change of reviews style

--- webapp/src/components/products/ReviewView.tsx
+++ webapp/src/components/products/ReviewView.tsx
@@ -1,6 +1,7 @@
 import { Rating, Typography } from "@mui/material";
 import { Product, Review } from "../../shared/shareddtypes";
-
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
 type ReviewViewProps = {
     review: Review;
 }
@@ -10,13 +11,17 @@ function ReviewView(props:ReviewViewProps): JSX.Element {
     return (
         <div>
             <div>
-                <Typography variant="subtitle1">{props.review.userId}</Typography>
+                <Typography variant="subtitle1">
+                <PersonOutlineIcon> </PersonOutlineIcon> {props.review.userId}
+                    </Typography>
             </div>
             <div>
-                <Rating name="read-only" value={props.review.rating} readOnly />
+             <ArrowRightIcon /> <Rating name="read-only" value={props.review.rating} readOnly />
             </div>
             <div>
-                <Typography variant="body1">{props.review.comment}</Typography>
+             <Typography variant="body1"> 
+              <ArrowRightIcon /> {props.review.comment}
+             </Typography>
             </div>
         </div>
     );
