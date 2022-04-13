0fec032a6a1df1210b56d9bc3b63a0dcadf91221
Luis Miguel Alonso <UO270139@uniovi.es>
2022-04-13 11:12:25 +0200

    Fixed las code smell

--- webapp/src/shared/shareddtypes.ts
+++ webapp/src/shared/shareddtypes.ts
@@ -1,29 +1,29 @@
 export type User = {
-    name:string;
-    email:string;
-  }
+  name: string;
+  email: string;
+}
 
 export type Product = {
-    id:string;
-    name:string;
-    description:string;
-    price:number;
-    image:string;
-    category:string;
-    numImages:number;
-    product: Product;
-    quantity: Number;
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  image: string;
+  category: string;
+  numImages: number;
+  product: Product;
+  quantity: number;
 }
 
 export type Order = {
   userId: string;
-    products: Array<Product>;
-    subTotal: number;
-    deliveryPrice: number;
-    createdAt: Date;
+  products: Array<Product>;
+  subTotal: number;
+  deliveryPrice: number;
+  createdAt: Date;
 }
 
 export type ItemCart = {
-  product:Product
+  product: Product
   quantity: number
 }
