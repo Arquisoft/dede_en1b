d54db6366d8d2fd31a046d38767dbf1120175b1e
Daniel <danielarezdiaz@gmail.com>
2022-04-24 22:51:36 +0200

    Added product upload with image

--- webapp/src/components/administrator/AdminView.tsx
+++ webapp/src/components/administrator/AdminView.tsx
@@ -7,8 +7,20 @@ import { Button, TextField } from '@mui/material';
 import { styled } from "@mui/material/styles";
 
 import { Card, Box, Divider} from "@mui/material";
+import { addProduct } from '../../api/api';
+import { Product } from '../../shared/shareddtypes';
 
 function AdminView(): JSX.Element {
+
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
+    const [price, setPrice] = useState(0);
+    const [category, setCategory] = useState("");
+    const [images, setImages] = useState<FileList>();
+
+    function newProduct() {
+        addProduct({name:name, description, price, category} as Product, images as FileList);
+    }
 
     return (
         <Box justifyContent="center">
@@ -24,6 +36,8 @@ function AdminView(): JSX.Element {
                         placeholder="Set Product Name"
                         style={{ width: "50%" }}
                         sx={{ input: { color: 'black' } }}
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
                     />
 
                     <TextField
@@ -33,15 +47,19 @@ function AdminView(): JSX.Element {
                         placeholder="Set Product Description"
                         style={{ width: "50%" }}
                         sx={{ input: { color: 'black' } }}
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
                     />
 
                     <TextField
                         variant="outlined"
-                        type="text"
+                        type="number"
                         id="product-price"
                         placeholder="Set Product Price"
                         style={{ width: "50%" }}
                         sx={{ input: { color: 'black' } }}
+                        value={price}
+                        onChange={(e) => setPrice(parseInt(e.target.value))}
                     />
 
                     <TextField
@@ -51,9 +69,17 @@ function AdminView(): JSX.Element {
                         placeholder="Set Product category"
                         style={{ width: "50%" }}
                         sx={{ input: { color: 'black' } }}
+                        value={category}
+                        onChange={(e) => setCategory(e.target.value)}
                     />
-                    <input type="submit" value = "Add" />
-                                   
+                    <Button variant="contained" component="label">
+                    Upload File
+                    <input type="file" hidden multiple onChange={(event)=>setImages(event.target.files as FileList)}/>
+                    </Button>
+
+                    <Button variant="contained" color="primary" onClick={newProduct}>
+                        Add Product
+                    </Button>            
                 </form>
 
                 <label> Delete product:</label>
