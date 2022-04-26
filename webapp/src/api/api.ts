de77aae7ec351414560dccb340b89567accc7973
Luis Miguel Alonso Ferreiro <UO270139@uniovi.es>
2022-04-26 11:48:23 +0200

    Reverted const4let

--- webapp/src/api/api.ts
+++ webapp/src/api/api.ts
@@ -1,9 +1,9 @@
-import { User, Product, Order,ItemCart, Review } from '../shared/shareddtypes';
+import { User, Product, Order, ItemCart, Review } from '../shared/shareddtypes';
 
 
 //if the current url is www.dedeen1b.tk sets the apiEndPoint to api.dedeen1b.tk
 export const baseApiEndPoint = window.location.href.includes("www.dedeen1b.tk") ? "https://api.dedeen1b.tk" : (process.env.REACT_APP_API_URI || 'http://localhost:5000');
-export let apiEndPoint = baseApiEndPoint + "/api"
+export const apiEndPoint = baseApiEndPoint + "/api"
 
 
 
@@ -20,20 +20,20 @@ export async function addUser(user: User): Promise<boolean> {
 }
 
 export async function getUsers(): Promise<User[]> {
-  let apiEndPoint = window.location.href.includes("www.dedeen1b.tk") ? "https://api.dedeen1b.tk/api" : (process.env.REACT_APP_API_URI || 'http://localhost:5000/api');
+  const apiEndPoint = window.location.href.includes("www.dedeen1b.tk") ? "https://api.dedeen1b.tk/api" : (process.env.REACT_APP_API_URI || 'http://localhost:5000/api');
   let response = await fetch(apiEndPoint + '/users/list');
   //The objects returned by the api are directly convertible to User objects
   return response.json()
 }
 
-export async function getProducts(searchParams?:String): Promise<Product[]> {
+export async function getProducts(searchParams?: String): Promise<Product[]> {
   let response = await fetch(apiEndPoint + '/products' + (searchParams ? '?search=' + searchParams : ''));
   return response.json();
 }
 
 export async function getOrderByUserId(webId: string): Promise<Order[]> {
-  let apiEndPoint = window.location.href.includes("www.dedeen1b.tk") ? "https://api.dedeen1b.tk/api" : (process.env.REACT_APP_API_URI || 'http://localhost:5000/api');
-  let response = await fetch(apiEndPoint + '/order/find' ,{
+  const apiEndPoint = window.location.href.includes("www.dedeen1b.tk") ? "https://api.dedeen1b.tk/api" : (process.env.REACT_APP_API_URI || 'http://localhost:5000/api');
+  let response = await fetch(apiEndPoint + '/order/find', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ 'webId': webId })
@@ -41,8 +41,8 @@ export async function getOrderByUserId(webId: string): Promise<Order[]> {
   return response.json();
 }
 
-export async function addReview(review:Review) {
-  let apiEndPoint = process.env.REACT_APP_API_URI || 'http://localhost:5000/api'
+export async function addReview(review: Review) {
+  const apiEndPoint = process.env.REACT_APP_API_URI || 'http://localhost:5000/api'
   let response = await fetch(apiEndPoint + '/review/add', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
@@ -52,41 +52,38 @@ export async function addReview(review:Review) {
     return true;
   else
     return false;
-  
+
 }
 
-export  function getCart() : ItemCart[] {
+export function getCart(): ItemCart[] {
   var cart = localStorage.getItem('cart');
-  if(cart != null)
+  if (cart != null)
     return JSON.parse(cart);
-  else{
+  else {
     localStorage.setItem('cart', JSON.stringify([]));
     return [];
   }
-    
+
 }
 
 
-export function getShippingCost(country?:String|null, locality?:String|null){
+export function getShippingCost(country?: String | null, locality?: String | null) {
   var cart = getCart();
   var totalPrice = cart.reduce((acc, item) => acc + item.product.price * item.quantity, 0);
-  var shippingCost;  
-  
-  if(country === "Spain" && locality !== "Ceuta" && locality !== "Melilla" && locality !== "Baleares"
-                  && locality !== "Canarias")
-  {
+  var shippingCost;
+
+  if (country === "Spain" && locality !== "Ceuta" && locality !== "Melilla" && locality !== "Baleares"
+    && locality !== "Canarias") {
     shippingCost = 3.99;
-  }else 
-  {
-    shippingCost = 7.50 ;
+  } else {
+    shippingCost = 7.50;
   }
 
-  if(country !== "Spain")
-  {
+  if (country !== "Spain") {
     shippingCost = 30;
   }
-  
-  if(totalPrice > 100){
+
+  if (totalPrice > 100) {
     shippingCost = 0;
   }
 
@@ -94,22 +91,22 @@ export function getShippingCost(country?:String|null, locality?:String|null){
 }
 
 
-export  function addToCart(itemCart:ItemCart, factor:number=1) : void {
+export function addToCart(itemCart: ItemCart, factor: number = 1): void {
   var cart = getCart();
   console.log(cart);
-  const index = cart.findIndex((i:ItemCart)=>i.product.id===itemCart.product.id);
-  if(index>=0){
+  const index = cart.findIndex((i: ItemCart) => i.product.id === itemCart.product.id);
+  if (index >= 0) {
     cart[index].quantity += factor;
-    }
+  }
   else
     cart.push(itemCart);
   localStorage.setItem('cart', JSON.stringify(cart));
 }
 
-export async function deleteFromCart(id:String) {
+export async function deleteFromCart(id: String) {
   var cart = getCart();
-  const index = cart.findIndex((i:ItemCart)=>i.product.id===id);
-  if(index>=0){
+  const index = cart.findIndex((i: ItemCart) => i.product.id === id);
+  if (index >= 0) {
     delete cart[index];
     cart = cart.filter(Boolean);
   }
@@ -120,46 +117,47 @@ export async function deleteFromCart(id:String) {
 }
 
 
-export function emptyCart(updateCarCountNumberFunction:Function) {
+export function emptyCart(updateCarCountNumberFunction: Function) {
   localStorage.setItem('cart', JSON.stringify([]));
   updateCarCountNumberFunction();
 }
 
 
-export async function getProductById(id: any):Promise<Product|undefined>{ 
-  let apiEndPoint = window.location.href.includes("www.dedeen1b.tk") ? "https://api.dedeen1b.tk/api" : (process.env.REACT_APP_API_URI || 'http://localhost:5000/api');
-  console.log(apiEndPoint+'/product/id')
-  let response = await fetch(apiEndPoint+'/products/' + id);
-  if(response.status === 200)
+export async function getProductById(id: any): Promise<Product | undefined> {
+  const apiEndPoint = window.location.href.includes("www.dedeen1b.tk") ? "https://api.dedeen1b.tk/api" : (process.env.REACT_APP_API_URI || 'http://localhost:5000/api');
+  console.log(apiEndPoint + '/product/id')
+  let response = await fetch(apiEndPoint + '/products/' + id);
+  if (response.status === 200)
     return response.json();
   else
     return undefined;
 }
 
-export async function getProductImages(id: string):Promise<string[]>{ 
-  console.log(apiEndPoint+'/product/' + id + '/images');
-  let response = await fetch(apiEndPoint+'/products/' + id + '/images');
+export async function getProductImages(id: string): Promise<string[]> {
+  console.log(apiEndPoint + '/product/' + id + '/images');
+  let response = await fetch(apiEndPoint + '/products/' + id + '/images');
   return response.json();
 }
 
 export async function addOrderToUser(webId: string) {
   console.log('adding order to user ' + webId)
   var shippingCost = getShippingCost(localStorage.getItem("country"), localStorage.getItem("locality"));
-  let apiEndPoint = window.location.href.includes("www.dedeen1b.tk") ? "https://api.dedeen1b.tk/api" : (process.env.REACT_APP_API_URI || 'http://localhost:5000/api');
+  const apiEndPoint = window.location.href.includes("www.dedeen1b.tk") ? "https://api.dedeen1b.tk/api" : (process.env.REACT_APP_API_URI || 'http://localhost:5000/api');
   let response = await fetch(apiEndPoint + '/order', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ 'userId': webId,'deliveryPrice': shippingCost,'products': 
-      getCart().map( (item:ItemCart) => {
-        let pid = item.product.id;
-        var product = {
-          'productId': pid,
-          'product': null,
-          'quantity': item.quantity,
-          'price': item.product.price
-        }
-        return product;
-      })
+    body: JSON.stringify({
+      'userId': webId, 'deliveryPrice': shippingCost, 'products':
+        getCart().map((item: ItemCart) => {
+          let pid = item.product.id;
+          var product = {
+            'productId': pid,
+            'product': null,
+            'quantity': item.quantity,
+            'price': item.product.price
+          }
+          return product;
+        })
     })
   });
   if (response.status === 200)
@@ -167,4 +165,4 @@ export async function addOrderToUser(webId: string) {
   else
     return false;
 }
-  
+
