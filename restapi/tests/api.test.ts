9e583731eab024867c97ea7953723b94d3baf620
UO258220 <UO258220@uniovi.es>
2022-04-26 02:29:24 +0200

    basic API tests, mongo randomly skipped (async?)

--- restapi/tests/api.test.ts
+++ restapi/tests/api.test.ts
@@ -3,6 +3,15 @@ import express, { Application } from 'express';
 import * as http from 'http';
 import bp from 'body-parser';
 import cors from 'cors';
+import mongoose  from "mongoose";
+import config from "../db/keys";
+
+import rproducts from '../routes/product.routes';
+import rusers from '../routes/user.routes';
+import rorders from '../routes/order.routes';
+
+// compose connection string from keys
+const conn = `mongodb://${config.DB_USER}:${config.DB_PASSWORD}@${config.DB_URI}/${config.DB_NAME}`;
 
 let app:Application;
 let server:http.Server;
@@ -15,6 +24,7 @@ beforeAll(async () => {
     };
     app.use(cors(options));
     app.use(bp.json());
+    app.use("/api", rproducts, rusers, rorders);
 
     server = app.listen(port, ():void => {
         console.log('Restapi server for testing listening on '+ port);
@@ -24,25 +34,147 @@ beforeAll(async () => {
 });
 
 afterAll(async () => {
-    server.close() //close the server
+    server.close()                  // close the server
 })
 
-describe('user ', () => {
+/** TESTS FOR USER (ADMIN) **/  // non-active for now
+/* describe('user ', () => {
+    it('can be registered', async () => {
+        const response:Response = await request(app).post("/register").send({
+            email: "test@email.com",
+            name: "test",
+            lastName: "testington",
+            password: "pass123",
+            PODUrl: "https://pod.inrupt.com/test/"
+        });
+        expect(response.statusCode).toBe(201);  // Success
+    });
+    //, 500000);    // increased timeout (?)
+
+    it('can log in', async () => {
+        const response:Response = await request(app).post("/login").send({
+            email: "test@email.com",
+            password: "pass123"
+        });
+        expect(response.statusCode).toBe(201);  // Success
+    });
+    //, 500000);    // increased timeout (?)
+
+    it('can be listed', async () => {
+        const response:Response = await request(app).get("/users");
+        expect(response.statusCode).toBe(200);
+    });
+}); */
+
+/** TESTS FOR PRODUCT **/
+describe('product ', () => {
+
+    test("CRASH", (done) => {
+        request(app).get("/").expect(400);
+    })
+
     /**
-     * Test that we can list users without any error.
+     * Test that we can list all products without any error.
      */
-    it('can be listed',async () => {
-        //const response:Response = await request(app).get("/api/users/list");
-        //expect(response.statusCode).toBe(200);
+    it('can be listed', async () => {
+        const response:Response = await request(app).get("/products");
+        // Resultado en formato JSON
+        expect(response.type).toEqual("application/json");
+        expect(response.statusCode).toBe(200);
     });
 
     /**
-     * Tests that a user can be created through the productService without throwing any errors.
+     * Test that we can add products without errors.
      */
-    it('can be created correctly', async () => {
-        // let username:string = 'Pablo'
-        // let email:string = 'gonzalezgpablo@uniovi.es'
-        // const response:Response = await request(app).post('/api/users/add').send({name: username,email: email}).set('Accept', 'application/json')
-        // expect(response.statusCode).toBe(200);
+    it('can be added', async () => {
+        let product:Object = {
+            id: '1432',
+            name: 'testProduct',
+            description: 'A product just for testing purposes',
+            price: 7.7,
+            image: 'no image',
+            category: 'testing only',
+            numImages: 1
+        }
+
+        const response:Response = await request(app).post("/product")
+            .send(product);
+        expect(response.statusCode).toBe(200);
+    });
+
+    /**
+     * Test that the added product can be accessed without errors.
+     */
+    it('can be added', async () => {
+        const response:Response = await request(app).get("/product/1432");
+        // Resultado en formato JSON
+        expect(response.type).toEqual("application/json");
+        expect(response.statusCode).toBe(200);
+    });
+});
+
+/** TESTS FOR ORDER **/
+describe('order ', () => {
+
+    /**
+     * Test that we get all the orders without errors
+     */
+     it('can be listed', async () => {
+        const response:Response = await request(app).get("/orders");
+        // Resultado en formato JSON
+        expect(response.type).toEqual("application/json");
+        expect(response.statusCode).toBe(200);
+    });
+
+    /**
+     * Test that we can save an empty order when completed with no errors.
+     */
+    it('can be saved', async () => {
+        let order:Object = {
+            userId: 'testingUID',
+            products: {},
+            subTotal: 555.0,
+            deliveryPrice: 570.6
+        }
+
+        const response:Response = await request(app).post("/order")
+            .send(order);
+        expect(response.statusCode).toBe(200);
+    });
+
+    /**
+     * Test that we can save a non-empty order when completed with no errors.
+     */
+    it('can be saved', async () => {
+        let order:Object = {
+            userId: 'testest',
+            products: {
+                id: '744',
+                name: 'orderTestP',
+                description: 'A product just for the order to have it',
+                price: 0.6,
+                image: 'no image',
+                category: 'testing only',
+                numImages: 1
+            },
+            subTotal: 78.0,
+            deliveryPrice: 134.5
+        }
+
+        const response:Response = await request(app).post("/order")
+            .send(order);
+        expect(response.statusCode).toBe(200);
+    });
+
+    /**
+     * Test that the added order can be accesed for the user.
+     */
+    it('can be added', async () => {
+        let userId = 'testingUID';
+        const response:Response = await request(app).post("/order/find")
+            .send(userId);
+        // Resultado en formato JSON
+        expect(response.type).toEqual("application/json");
+        expect(response.statusCode).toBe(200);
     });
 });
