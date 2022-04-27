f2298f05bec4e750682abe03aa2e14725ddbbc2d
Jesus Gonzalez Mendez <jesugm1@gmail.com>
2022-04-27 12:29:12 +0200

    code smells

--- webapp/src/components/administrator/OrderAdminDetails.tsx
+++ webapp/src/components/administrator/OrderAdminDetails.tsx
@@ -1,9 +1,8 @@
 
-import { styled } from '@mui/material/styles';
+
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
-import IconButton, { IconButtonProps } from '@mui/material/IconButton';
 
 import { baseApiEndPoint } from '../../api/api';
 
