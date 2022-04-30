4250d7f270e02515926191736159d90ee45ac534
Daniel <danielarezdiaz@gmail.com>
2022-04-30 19:10:46 +0200

    Revert "Fix shopping cart product image not updating"
    This reverts commit f1064bf2b0b957417a740c62d53be9e96ab6bc36.

--- webapp/src/components/cart/CartItem.tsx
+++ webapp/src/components/cart/CartItem.tsx
@@ -35,7 +35,7 @@ function CartItem(props: CartItemProps) {
     useEffect(() => {
         setQuantity(props.item.quantity);
         getImage();
-    }, [props.item]);
+    }, [props.item.quantity]);
 
 
 
